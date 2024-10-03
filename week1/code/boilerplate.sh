d5c5ef61b6bc7c3557fdd2346e9116b5bf7c0258
Laiyin Zhou <l.zhou24@imperial.ac.uk>
2024-10-03 12:16:19 +0100

    Full first commit of new project

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Author: l.zhou@imperial.ac.uk
+#Script: boilerplate.sh
+#Desc: simple boilerplate for shell scripts
+#Arguments: none
+#Date: Oct 2024
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
